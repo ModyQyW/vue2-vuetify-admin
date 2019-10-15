d01ea91383ba63d80d4bb3418e9141caf9d49d22
ModyQyW <261857860@qq.com>
2019-10-15 09:59:51 +0800

    remove i18n.js clg

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -5,14 +5,12 @@ Vue.use(VueI18n)
 
 function loadLocaleMessages () {
   const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  console.log('locales', locales.keys())
   const messages = {}
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
       messages[locale] = locales(key)
-      console.log('locales', locales(key))
     }
   })
   return messages
